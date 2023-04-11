5b72c3d4d0403c6d6fc2583ce2608bd3970b5dae
Mohamed Chakib Chiboub <mchiboub@student.42perpignan.fr>
2023-04-11 14:55:49 +0200

    updated client to work with 8 bits not 32

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mchiboub <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:45:06 by mchiboub          #+#    #+#             */
-/*   Updated: 2023/04/10 18:15:20 by mchiboub         ###   ########.fr       */
+/*   Updated: 2023/04/11 14:54:40 by mchiboub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ void	char2bin(unsigned int nbr, char *str)
 	int		i;
 	int		temp;
 
-	i = 31;
+	if (nbr / 4294967040 == 1)
+		nbr -= 4294967040;
+	i = 7;
 	while (i >= 0)
 	{
 		temp = nbr / ft_recursive_power(2, i);
-		str[31 - i] = 48 + temp;
+		str[7 - i] = 48 + temp;
 		if (temp)
 			nbr -= ft_recursive_power(2, i);
 		i--;
 	}
-	str[32] = '\0';
+	str[8] = '\0';
 }
 
 void	handler(int sig)
@@ -66,7 +68,7 @@ void	client_work(char *strbin, int pid, int i)
 int	main(int argc, char *argv[])
 {
 	int		pid;
-	char	strbin[33];
+	char	strbin[9];
 	int		j;
 
 	if (argc != 3)
@@ -81,7 +83,7 @@ int	main(int argc, char *argv[])
 	while (1)
 	{
 		char2bin(argv[2][j], &strbin[0]);
-		client_work(&strbin[24], pid, 0);
+		client_work(&strbin[0], pid, 0);
 		if (argv[2][j] == '\0')
 			break ;
 		j++;
